b7a671a9679d9d79fa98fe7b63809fbfedd30180
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-14 07:03:39 +0530

    Create expense project instances

---
+++ expense-v1/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-06b8bece9ed37b3dc"]
+  tags = {
+    Name = "frontend"
+  }
+}
+
+
+
+resource "aws_instance" "backend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-06b8bece9ed37b3dc"]
+  tags = {
+    Name = "backend"
+  }
+}
+
+
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-06b8bece9ed37b3dc"]
+  tags = {
+    Name = "backend"
+  }
+}
+
