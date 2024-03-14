82906ebad1be3aaefb8d34ecf19f03318c488ae8
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-14 07:20:34 +0530

    Vars demo

--- 03-variables/var.tf
+++ 03-variables/var.tf
@@ -7,9 +7,30 @@ variable "x" {
 # Declare variable without value
 #variable "y" {}
 
+variable "y_list" {
+  default = [1,2,3]
+}
+
+variable "z_map" {
+  default = {
+    x = 10
+    y = 20
+  }
+}
 
 # Print variable
 output "x" {
   value = var.x
 }
+
+output "y" {
+  value = var.y_list
+}
+
+output "z" {
+  value = var.z_map
+}
+
+
+
 
